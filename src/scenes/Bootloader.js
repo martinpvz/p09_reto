623a324f83b4daf92281401d1ab735c31bac24ac
ManuelARs <m.a.r.s.232001@gmail.com>
2022-10-25 19:22:20 -0500

    Fisicas1

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -20,14 +20,6 @@ class Bootloader extends Phaser.Scene{
         this.javier.body.setOffset(180,0);
         this.javier.body.setMass(1);
 
-        this.barraArriba = this.add.image(160, 170, 'barraArriba1');
-        this.barraArriba.setScale(0.3)
-        this.barraArriba2 = this.add.image(430, 170, 'barraArriba2');
-        this.barraArriba2.setScale(0.3)
-        this.barraArriba3 = this.add.image(740, 170, 'barraArriba3');
-        this.barraArriba3.setScale(0.3)
-        this.barraArriba4 = this.add.image(1000, 170, 'barraArriba4');
-        this.barraArriba4.setScale(0.3)
         this.barraArriba5 = this.add.image(1230, 170, 'barraArriba1');
         this.barraArriba5.setScale(0.3)
         this.barraPuerta = this.add.image(1460, 230, 'barraPuerta');
@@ -43,24 +35,9 @@ class Bootloader extends Phaser.Scene{
 
         var torres = this.physics.add.staticGroup();
         var barraTiempo = this.physics.add.staticGroup();
-        // this.torre1 = this.add.image(150, 675, 'torre1');
-        // this.torre1.setScale(0.3)
-        // this.torre2 = this.add.image(300, 675, 'torre2');
-        // this.torre2.setScale(0.3)
-        // this.torre3 = this.add.image(450, 675, 'torre3');
-        // this.torre3.setScale(0.3)
-        // this.torre4 = this.add.image(600, 675, 'torre4');
-        // this.torre4.setScale(0.3)
-        // this.torre5 = this.add.image(780, 675, 'torre5');
-        // this.torre5.setScale(0.3);
-        // this.barraTiempo1 = this.add.image(980, 650, 'barraTiempo');
-        // this.barraTiempo1.setScale(0.3);
-        // this.barraTiempo2 = this.add.image(1120, 600, 'barraTiempo');
-        // this.barraTiempo2.setScale(0.3);
-        // this.barraTiempo3 = this.add.image(1260, 550, 'barraTiempo');
-        // this.barraTiempo3.setScale(0.3);
-        // this.barraTiempo4 = this.add.image(1400, 500, 'barraTiempo');
-        // this.barraTiempo4.setScale(0.3);
+        var barraTorre = this.physics.add.staticGroup();
+        var barrasArriba = this.physics.add.staticGroup()
+        var barrasHielo = this.physics.add.staticGroup()
 
         torres.create(150, 715, 'torre1').setScale(0.2).refreshBody();
         torres.create(300, 715, 'torre2').setScale(0.2).refreshBody();
@@ -68,29 +45,24 @@ class Bootloader extends Phaser.Scene{
         torres.create(600, 715, 'torre4').setScale(0.2).refreshBody();
         torres.create(780, 715, 'torre5').setScale(0.2).refreshBody();
 
-        barraTiempo.create(980, 650, 'barraTiempo').setScale(0.3).refreshBody();
-        barraTiempo.create(1120, 600, 'barraTiempo').setScale(0.3).refreshBody();
-        barraTiempo.create(1260, 550, 'barraTiempo').setScale(0.3).refreshBody();
-        barraTiempo.create(1400, 500, 'barraTiempo').setScale(0.3).refreshBody();
+        barraTiempo.create(980, 670, 'barraTiempo').setScale(0.3).refreshBody();
+        barraTiempo.create(1120, 620, 'barraTiempo').setScale(0.3).refreshBody();
+        barraTiempo.create(1260, 570, 'barraTiempo').setScale(0.3).refreshBody();
+        barraTiempo.create(1400, 520, 'barraTiempo').setScale(0.3).refreshBody();
 
+        barraTorre.create(840, 585, 'barraElevador').setDepth(-1).setScale(0.2).refreshBody();
 
-        // this.physics.add.existing(this.torre1, true );
-        // this.physics.add.existing(this.torre2, true );
-        // this.physics.add.existing(this.torre3, true );
-        // this.physics.add.existing(this.torre4, true );
-        // this.physics.add.existing(this.torre5, true );
-        // this.physics.add.existing(this.barraTiempo1, true );
-        // this.physics.add.existing(this.barraTiempo2, true );
-        // this.physics.add.existing(this.barraTiempo3, true );
-        // this.physics.add.existing(this.barraTiempo4, true );
+        barrasArriba.create(160,170, 'barraArriba1').setScale(0.3).refreshBody()
+        barrasArriba.create(430,170, 'barraArriba2').setScale(0.3).refreshBody()
 
-        this.barraTorre = this.add.image(880, 480, 'barraElevador');
-        this.barraTorre.setScale(0.3);
+        barrasHielo.create(740,170, 'barraArriba3').setScale(0.3).refreshBody()
+        barrasHielo.create(1000,170, 'barraArriba4').setScale(0.3).refreshBody()
+
         this.picos = this.add.image(1200, 735, 'picos');
         this.picos.setScale(0.3);
         
         this.escalar = this.add.image(1120, 365, 'escalar');
-        this.escalar.setScale(0.3);
+        this.escalar.setScale(0.28);
         this.barraDiagonal = this.add.image(1510, 430, 'barraDiagonal');
         this.barraDiagonal.setScale(0.3);
         this.escalera = this.add.image(262, 230, 'escalera');
@@ -104,17 +76,17 @@ class Bootloader extends Phaser.Scene{
         this.cursors = this.input.keyboard.createCursorKeys();
         
         this.physics.add.existing(this.escalar, true );
-        this.physics.add.existing(this.barraArriba, true );
-        this.physics.add.existing(this.barraArriba2, true );
-        this.physics.add.existing(this.barraArriba3, true );
-        this.physics.add.existing(this.barraArriba4, true );
+        //this.physics.add.existing(this.barraArriba, true );
+        //this.physics.add.existing(this.barraArriba2, true );
+        //this.physics.add.existing(this.barraArriba3, true );
+        //this.physics.add.existing(this.barraArriba4, true );
         this.physics.add.existing(this.barraArriba5, true );
         this.physics.add.existing(this.escalera, true );
         this.physics.add.existing(this.barraCF1, true );
         this.physics.add.existing(this.barraCF2, true );
         this.physics.add.existing(this.cuerda, true );
         this.physics.add.existing(this.picos, true );
-        this.physics.add.existing(this.barraTorre, true );
+        //this.physics.add.existing(this.barraTorre, true );
         this.physics.add.existing(this.barraDiagonal, true );
         this.physics.add.existing(this.puerta, true );
         this.physics.add.existing(this.barraPuerta, true );
@@ -125,15 +97,38 @@ class Bootloader extends Phaser.Scene{
         //     this.javier.setAcceleration(0);
         // });
         this.physics.add.collider(this.javier, torres, () => {
-                this.javier.setVelocity(0);
-                this.javier.setAcceleration(0);
-                // this.javier.body.onFloor()=true;
-            });
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
         this.physics.add.collider(this.javier, barraTiempo, () => {
-                this.javier.setVelocity(0);
-                this.javier.setAcceleration(0);
-                // this.javier.body.onFloor()=true;
-            });
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, barraTorre, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, this.barraDiagonal, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, this.escalar, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, barrasArriba, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, barrasHielo, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+        this.physics.add.collider(this.javier, this.barraPuerta, () => {
+            this.javier.setVelocityX(0);
+            this.javier.setAccelerationX(0);
+        });
+
     }
 
 
